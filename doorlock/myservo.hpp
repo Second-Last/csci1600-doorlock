f34ef85d6f9dc3c594661abade57a252e8d194bf
Ethan Ye <ethan.ye0312@gmail.com>
2025-12-08 22:58:18 -0500

    Finished adding comments to myservo.hpp

--- doorlock/myservo.hpp
+++ doorlock/myservo.hpp
@@ -29,10 +29,25 @@ struct MyServo {
   MyServo(int servoPin, int feedbackPin, int transistorPin)
       : servoPin(servoPin), feedbackPin(feedbackPin), transistorPin(transistorPin) {}
 
-  // Initialize the necessary hardware.
+  /**
+   * This function initializes the necessary hardware so that the transistor pin can output signals
+   * 
+   * Input: None
+   * Output: None
+   * 
+   */
   void init() { pinMode(transistorPin, OUTPUT); }
 
-  // Attach to the servo.
+  /**
+   * This function attaches the servo motor to the servoPin, allowing the servor motor to be controlled
+   * by sending signals through this pin.
+   * 
+   * Input: None
+   * Output: None
+   * 
+   * Side Effect: Servo motor hardware is configured
+   * 
+   */
   void attach() {
     if (attached) return;
     digitalWrite(transistorPin, HIGH);
@@ -40,22 +55,50 @@ struct MyServo {
     attached = true;
   }
 
-  // Send a request to the servo to turn to the given degree. Assumes that the
-  // servo is already attached. Note that the servo cannot guarantee it would've
-  // turned to the requested position when this method returns.
+  /**
+   * This function formally sends a request to the servo motor to turn to a given degree. It assumes that
+   * the servo motor is already attached to corresponding hardware. Note that the servo cannot guarantee it would've 
+   * turned to the requested position when this method returns.
+   * 
+   * Input:
+   *  - deg (int) : Integer value indicating the desired degree to which the servo motor should be turned
+   * 
+   * Output: None
+   * 
+   * Side Effect: Servo motor should rotate to desired degree
+   * 
+   */
   void write(int deg) {
     assert(attached);
     servo.write(deg);
   }
 
-  // Same as `write` but attaches the servo if it has not been attached.
+  /**
+   * This function accomplishes the same as the `write()` function above, but attaches the motor to the 
+   * correct hardware before attempting to rotate the motor
+   * 
+   * Input:
+   *  - deg (int) : Integer value indicating the desired angle to which the servo motor should be rotated
+   * 
+   * Output: None
+   * 
+   * Side Effect: Servo motor should rotate to specified degree
+   * 
+   */
   void attachAndWrite(int deg) {
     attach();
     write(deg);
   }
 
-  // Detach the servo by cutting its power supply. This means the motor can no
-  // longer control itself.
+  /**
+   * This function detaches the servo motor by cutting off its power supply. Consequently, any calls to `write()`
+   * will no longer rotate the motor
+   * 
+   * Input: None
+   * 
+   * Output: None
+   * 
+   */
   void detach() {
     if (!attached) return;
     servo.detach();
@@ -63,7 +106,14 @@ struct MyServo {
     attached = false;
   }
 
-  // Return the current position of the motor.
+  /**
+   * This function returns the current position of the motor in degrees
+   * 
+   * Input: None
+   * 
+   * Output: Integer value representing the current motor's position, in degrees
+   * 
+   */
   int deg() {
     int feedback = analogReadStable(feedbackPin);
     if (attached) {
@@ -73,13 +123,25 @@ struct MyServo {
     }
   }
 
-  // Establish the feedback values for 2 positions of the servo.
-  // With this information, we can interpolate feedback values for intermediate positions
+  /**
+   * This function is responsible for calibrating the servor motor by setting the min and max positions.
+   * This function establishes the feedback values for 2 positions of the servo motor. Such information
+   * is relevant to enable us to interpolate feedback values for intermediate positions.
+   * 
+   * Input:
+   *  - minPos (int) : The min position of the servo motor in degrees, represented by an int
+   *  - maxPos (int) : The max position of the servo motor in degrees, represented by an int
+   * 
+   * Output: None
+   * 
+   * Side Effect: Servo motor is calibrated with its min and max positions 
+   * 
+   */
   void calibrate(int minPos, int maxPos) {
-	Serial.print("Calibrating with minPos=");
-	Serial.print(minPos);
-	Serial.print(", maxPos=");
-	Serial.println(maxPos);
+    Serial.print("Calibrating with minPos=");
+    Serial.print(minPos);
+    Serial.print(", maxPos=");
+    Serial.println(maxPos);
 
     bool prevAttached = attached;
 
